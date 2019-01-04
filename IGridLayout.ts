ace9f9cc9daa38a3b63ad88997937096a5485817
eelke <eelkevanderbij.prive+github@gmail.com>
2019-01-04 15:09:13 -0300

    Add GridLayout.ts

---
+++ IGridLayout.ts
@@ -0,0 +1,17 @@
+export interface Breakpoint {
+  threshold: number;
+  columns?: number;
+  gutter?: number;
+  color?: string;
+  padding?: number;
+}
+
+export interface Options {
+  columns: number;
+  gutter: number;
+  maxWidth: number;
+  color: string;
+  padding: number;
+  className?: string;
+  breakpoints: Array<Breakpoint>;
+}
